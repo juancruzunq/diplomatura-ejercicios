59928f9031e81af8ca3cf59a30eb79cb22fafa57
juancruzunq <juancruz.unq@gmail.com>
2023-08-28 17:59:45 -0300

    Search casi terminado

--- Trabajo Practico/frontend/src/components/layout/MascotaCard.js
+++ Trabajo Practico/frontend/src/components/layout/MascotaCard.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import '../../styles/components/layout/mascotaCard.css';
 import Imagen1 from '../../images/adopciones/kiza.png';
 
-import { FaMapMarkerAlt, FaHeart, FaWhatsapp } from 'react-icons/fa';
+import { FaMapMarkerAlt, FaInfoCircle, FaWhatsapp } from 'react-icons/fa';
 
 const MascotaCard = ({ mascota }) => {
-  const descripcionClassName =
-    mascota.descripcion.length > 100 ? 'descripcion scrollable' : 'descripcion';
+  const descripcionClassName =mascota.descripcion.length > 100 ? 'descripcion scrollable' : 'descripcion';
+  const atencionMedica = () => {
+    if(mascota.vacunado && mascota.castrado)
+    return "Vacunado & Castrado";
+    if(mascota.vacunado || mascota.castrado)
+    return mascota.vacunado ? "Vacunado" : "Castrado";
+  else return "Sin atencion medica"
+  };
 
   return (
     <div className="card">
@@ -17,16 +23,23 @@ const MascotaCard = ({ mascota }) => {
         </div>
         <div className="adopciones-card-info">
           <h4 className="nombre-mascota">{mascota.nombre}</h4>
+          <div className="additional-info">
+              <FaInfoCircle className="additional-info-icon" />
+              <p className="additional-info-text">{mascota.tipo === 0 ? 'Hembra, ' : 'Macho, '}{mascota.edad === 0 ? 'Cachorro' : 'Adulto '}</p>       
+          </div>
+          <div className="additional-info">
+              <FaInfoCircle className="additional-info-icon" />
+              <p className="additional-info-text">{atencionMedica()}</p>       
+          </div>            
           <p className={descripcionClassName}>{mascota.descripcion}</p>
           <div className="location-container">
             <FaMapMarkerAlt className="location-icon" />
-            <p>{mascota.provincia}</p>
+            <p>{mascota.provincia}</p>            
           </div>
         </div>
       </div>
       <div className="icons-container">
         <FaWhatsapp className="icon whatsapp" />
-        <FaHeart className="icon" />
       </div>
     </div>
   );
